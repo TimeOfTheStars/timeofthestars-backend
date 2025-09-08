fd647c65793c8e788c002a5d53fbdaf334f1b17b
Alexey <Arcadnick@yandex.ru>
2025-09-08 18:45:38 +0300

    upd tournament

--- database/seeders/TournamentSeeder.php
+++ database/seeders/TournamentSeeder.php
@@ -10,21 +10,13 @@ class TournamentSeeder extends Seeder
     public function run(): void
     {
         Tournament::create([
-            'name' => 'Товарищеский турнир',
-            'start_date' => '2024-09-04',
-            'end_date' => '2024-10-01',
+            'name' => 'Предсезонный турнир',
+            'start_date' => '2025-09-09',
+            'end_date' => null,
             'location' => 'СК "Торпедо"',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
 
-//        Tournament::create([
-//            'name' => 'Кубок победы',
-//            'start_date' => '2025-05-01',
-//            'end_date' => '2025-05-04',
-//            'location' => '"ГУОР"',
-//            'created_at' => now(),
-//            'updated_at' => now(),
-//        ]);
     }
 }
