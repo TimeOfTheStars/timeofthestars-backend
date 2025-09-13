b49d298900636639f959fab346a73fbfd0e65358
Alexey <Arcadnick@yandex.ru>
2025-09-13 17:27:21 +0300

    thirrd match

--- database/seeders/GameMatchSeeder.php
+++ database/seeders/GameMatchSeeder.php
@@ -44,8 +44,8 @@ class GameMatchSeeder extends Seeder
                 'date' => '2025-09-12',
                 'time' => '20:00:00',
                 'location' => 'СК "Торпедо"',
-                'score_team_a' => null,
-                'score_team_b' => null
+                'score_team_a' => 13,
+                'score_team_b' => 1
             ],
 //----------------------------------------------------------------
             [
