03ebafa7b3c36e8cdeab97eecb806146ca6524ae
Alexey <Arcadnick@yandex.ru>
2025-09-19 22:26:51 +0300

    score 6 match

--- database/seeders/GameMatchSeeder.php
+++ database/seeders/GameMatchSeeder.php
@@ -72,8 +72,8 @@ class GameMatchSeeder extends Seeder
                 'date' => '2025-09-19',
                 'time' => '20:00:00',
                 'location' => 'СК "Торпедо"',
-                'score_team_a' => null,
-                'score_team_b' => null
+                'score_team_a' => 9,
+                'score_team_b' => 3
             ],
             //----------------------------------------------------------------
             [
